93e6be62789387f58b2bd90aa33e3145d78d7e49
unknown <fishermujahid@gmail.com>
2024-03-25 12:49:11 +0200

    finished

--- code.js
+++ code.js
@@ -1,10 +1,65 @@
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 3000
-require("./config/route")
+const express = require("express");
+const app = express();
+require("./config/route");
+require("./models/connectDB");
+const excel = require("exceljs");
+const bodyParser = require("body-parser");
+const User = require("./models/connectDB");
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static("./static"), express.json())
+app.use(bodyParser.json());
+app.use(express.static("./static"));
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  try {
+    const { firstName, lastName, percentage } = req.body;
+
+    const newUser = new User({
+      firstName: firstName,
+      lastName: lastName,
+      percentage: percentage
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/download-excel", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const workbook = new excel.Workbook();
+    const worksheet = workbook.addWorksheet("Users");
+
+    worksheet.addRow(["First Name", "Last Name", "Percentage"]);
+
+    users.forEach((user) => {
+      worksheet.addRow([user.firstName, user.lastName, user.percentage]);
+    });
+
+    const buffer = await workbook.xlsx.writeBuffer();
+
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="userdata.xlsx"'
+    );
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.send(buffer);
+  } catch (error) {
+    console.error("Error downloading Excel file:", error);
+    res.status(500).send("Internal server error");
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`server = ${PORT}😎`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ models/connectDB.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const SchemaMain = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  percentage: {
+    type: String,
+    required: true,
+  }
+});
+
+const User = mongoose.model("User", SchemaMain);
+module.exports = User;
